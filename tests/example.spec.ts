bf5ebc59e6b5454c159c767bc25d345942bbc531
Mart Haamer <49863634+Mart556@users.noreply.github.com>
2025-10-31 12:07:36 +0200

    Initialize Playwright testing framework with configuration, dependencies, and example tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+const PAGE_URL = "http://37.27.45.218:81/";
+
+test("has leaderboard", async ({ page }) => {
+	await page.goto(PAGE_URL);
+
+	await expect(page.getByRole("heading", { name: "Edetabel" })).toBeVisible();
+});
+
+test("can log in as user", async ({ page }) => {
+	await page.goto(PAGE_URL);
+
+	await page.getByRole("button", { name: "Logi Sisse" }).click();
+
+	await page.getByLabel("Kasutajanimi").fill("Mart");
+	await page.getByLabel("Parool").fill("qwerty");
+
+	await page.getByRole("button", { name: "Logi Sisse" }).click();
+
+	await expect(page.getByRole("button", { name: "Logi Välja" })).toBeVisible();
+});
+
+test("can choose difficulty", async ({ page }) => {
+	await page.goto(PAGE_URL);
+
+	await page.getByRole("button", { name: "Logi Sisse" }).click();
+
+	await page.getByLabel("Kasutajanimi").fill("Mart");
+	await page.getByLabel("Parool").fill("qwerty");
+
+	await page.getByRole("button", { name: "Logi Sisse" }).click();
+
+	await page.getByRole("button", { name: "Keskmine" }).click();
+
+	const selector = page.getByRole("button", { name: "Keskmine" });
+	await expect(selector).toHaveClass(/bg-green-500/);
+
+	await page.getByRole("button", { name: "Mängima" }).click();
+
+	await expect(page.getByText("Aega jäänud")).toBeVisible();
+});
