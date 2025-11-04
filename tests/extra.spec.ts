3e76b38fd0dd8611808513aa16d89d09743a93b4
Mart Haamer <49863634+Mart556@users.noreply.github.com>
2025-11-04 11:27:12 +0200

    xd

--- tests/extra.spec.ts
+++ tests/extra.spec.ts
@@ -14,7 +14,7 @@ import { test, expect } from "@playwright/test";
 	await page.screenshot({ path: "tunniplaan.png", fullPage: true });
 }); */
 
-test("check Aly Valvas timetable", async ({ page }) => {
+/* test("check Aly Valvas timetable", async ({ page }) => {
 	await page.goto(PAGE_URL);
 
 	await page.waitForTimeout(1000);
@@ -30,4 +30,24 @@ test("check Aly Valvas timetable", async ({ page }) => {
 	await page.waitForTimeout(2000);
 
 	await page.screenshot({ path: "aly_valvas.png", fullPage: true });
+});
+
+test("test tunniplaan performance", async ({ page }) => {
+	const start = performance.now();
+	await page.goto(PAGE_URL);
+	const end = performance.now();
+	const loadTime = end - start;
+	console.log(`Page load time: ${loadTime} ms`);
+	expect(loadTime).toBeLessThan(3000);
+}); */
+
+const PAGE_URL2 = "https://siseveeb.voco.ee/";
+
+test("false credentials login test", async ({ page }) => {
+	await page.goto(PAGE_URL2);
+	await page.locator("#username").fill("ValeKasutaja");
+	await page.locator("#password").fill("ValeParool");
+	await page.locator("#form_submit").click();
+	await page.waitForTimeout(1000);
+	await page.screenshot({ path: "false.png", fullPage: true });
 });
